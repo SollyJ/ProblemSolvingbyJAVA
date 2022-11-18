5da50a5ffa4b9755a72334f65f2fe988b6539511
sollyj <jeongpine14@gmail.com>
2022-11-18 18:56:20 +0900

    1118

--- week1/sumOfSequence.java
+++ week1/sumOfSequence.java
@@ -1,4 +1,4 @@
-//backjoon 1024
+// baekjoon 1024
 import java.util.*;
 
 public class sumOfSequence {
@@ -8,18 +8,21 @@ public class sumOfSequence {
         long n = scan.nextInt();
         long l = scan.nextInt();
         long start_num = 0;
+        long temp_sum = 0;
         boolean flag = true;
 
         while(l <= 100) {
-            start_num = n/l - (l-1)/2;
+            start_num = n/l - (l-1)/2;  // 처음 시작 수를 구한다
 
             if(start_num < 0) {
                 break;
             }
+
+            temp_sum = (start_num * 2 + l - 1) * l / 2; // 처음시작수+1씩 한것을 l만큼 더한값
             
-            if(n == (start_num * 2 + l - 1) * l / 2) {
+            if(n == temp_sum) {
                 for(long k=0; k<l; k++)  {
-                    System.out.print(start_num+k + " ");
+                    System.out.print(start_num+k + " ");    // 프린트
                 }
                 flag = false;
                 break;
