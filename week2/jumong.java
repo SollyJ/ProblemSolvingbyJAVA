ce0e54fcf16aef69774c751f25f6738a7e3e6eec
sollyj <jeongpine14@gmail.com>
2022-11-26 14:18:49 +0900

    1126

---
+++ week2/jumong.java
@@ -0,0 +1,30 @@
+// Baekjoon_1940_주몽
+package week2;
+
+import java.io.*;
+import java.util.*;
+
+public class jumong {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {    
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i : arr) {
+            int one = i;
+            int two = M-one;
+            if(Arrays.stream(arr).anyMatch(x -> x == two))    answer++;
+        }        
+
+        System.out.print(answer/2);
+    }
+}
