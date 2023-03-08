b456a1e511d745c32f3f0df452c65559f6f73287
sollyj <jeongpine14@gmail.com>
2023-03-08 15:56:12 +0900

    :sparkles:TIL: 직사각형 별찍기 - Stream

---
+++ ProgrammersLv1/RectangleStar.java
@@ -0,0 +1,13 @@
+// Programmers_직사각형별찍기
+package ProgrammersLv1;
+
+import java.util.stream.IntStream;
+
+public class RectangleStar {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		IntStream.range(0, 5).forEach(s -> sb.append("*"));
+		IntStream.range(0, 3).forEach(s -> System.out.println(sb.toString()));
+	}
+}
