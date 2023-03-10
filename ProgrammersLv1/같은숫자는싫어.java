01191627ea501f8f71e260e90b3739c8343e5d86
sollyj <jeongpine14@gmail.com>
2023-03-10 19:54:51 +0900

    :sparkles:TIL: 같은 숫자는 싫어 - stack

---
+++ ProgrammersLv1/같은숫자는싫어.java
@@ -0,0 +1,32 @@
+// Programmers_같은숫자는싫어
+package ProgrammersLv1;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class 같은숫자는싫어 {
+	public static void main(String[] args) {
+		int[] arr = {3, 3, 4, 5, 6, 6, 1};
+
+		System.out.println(Arrays.toString(solution(arr)));
+	}
+
+	private static int[] solution(int[] arr) {
+		Stack<Integer> stack = new Stack<>();
+
+		stack.push(arr[0]);
+
+		for (int e : arr) {
+			if (stack.peek() != e)
+				stack.push(e);
+		}
+
+		int[] answer = new int[stack.size()];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = stack.get(i);
+		}
+
+		return answer;
+	}
+}
